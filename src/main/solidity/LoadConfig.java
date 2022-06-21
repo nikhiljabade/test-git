a659bc401d4e6f20d3e4999563e2224b7bb198cf
nikhiljabade <nikhil.jabade@gmail.com>
2022-06-21 20:01:42 +0530

    Added comments

--- src/main/solidity/LoadConfig.java
+++ src/main/solidity/LoadConfig.java
@@ -1,5 +1,6 @@
 public class LoadConfig {
     public static void main(String[] args) {
+        /* ADDED COMMENT */
         Properties prop = new Properties();
         prop.load(App.class.getClassLoader().getResourceAsStream("./resources/config.properties"));
         System.out.println(prop.getProperty("user"));
